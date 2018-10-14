f7bcdef6846d5c3e2478dc7508769c7d2454608d
rg3915 <rg3915@yahoo.com.br>
2018-10-14 01:43:49 -0300

    frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Navbar/>
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+    <Users/>
+  </div>
+</template>
+
+<script>
+
+import Navbar from './components/Navbar.vue'
+import Users from './components/Users'
+
+export default {
+  name: 'app',
+  components: {
+    Navbar,
+    Users
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /*margin-top: 60px;*/
+}
+</style>
---
+++ frontend/src/components/Users.vue
@@ -0,0 +1,49 @@
+<template>
+  <b-table striped hover :items="users"></b-table>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      headers: [
+        { text: 'Username', value: 'username' },
+        { text: 'E-mail', value: 'email' }
+      ],
+      users: []
+    }
+  },
+  created () {
+    axios.get('http://localhost:8000/api/user/json/')
+    .then(result => {
+      console.log(result)
+      this.users = result.data.data
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
